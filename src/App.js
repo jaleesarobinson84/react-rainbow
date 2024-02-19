47049c406f51f170d642ff6553a6a17b24daba01
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-02-19 17:19:06 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, {useState} from 'react';
+
+function App() {
+  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
