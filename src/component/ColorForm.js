ee668720a10f19a34939c29f74db505419309493
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-02-19 19:42:53 -0500

    assignment completed

---
+++ src/component/ColorForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function ColorForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.addColor(input);
+        setInput(''); // Clear input after submission
+    };
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    value={input}
+                    onChange={handleChange}
+                    placeholder="Hey!!! Color Here!!!..."
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default ColorForm;
